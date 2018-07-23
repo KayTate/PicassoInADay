f56c0ca8959c928ce025e7d622b69560092fc03a
tjyoung99 <40397667+tjyoung99@users.noreply.github.com>
2018-07-23 14:42:31 -0500

    added

--- PicassoInADay.pyde
+++ PicassoInADay.pyde
@@ -1,9 +1,10 @@
 from colorSpace import *
 from Penguin import *
+from Title import *
+
+page = "Title"
 
 def setup():
     size(700,550)
     
 def draw():
-    colorSpace()
-    showPenguin()
---
+++ Title.py
@@ -0,0 +1,20 @@
+def titlePage():
+    background(255)
+    
+    #Header
+    fill("#CA7AE0")
+    noStroke()
+    rect(0,0,700,100)
+    
+    fill(0)
+    titleFont = loadFont("CurlzMT-48.vlw")
+    textFont(titleFont)
+    textSize(100)
+    text("Picasso in a Day", 45, 80)
+    
+    
+    #Penguin Box
+    penguin = loadImage("penguin.PNG")
+    stroke(0)
+    rect(49,199,151,151)
+    image(penguin,50,200,150,150)
